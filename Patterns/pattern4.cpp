9759906cf5f2b397b1db5df5604ba54e8f71bcf4
pratyush1279 <pratyushpatwal@gmail.com>
2026-06-25 18:12:02 +0530

    message

--- Patterns/pattern4.cpp
+++ Patterns/pattern4.cpp
@@ -21,7 +21,62 @@ int pyramidPattern(){
     return 0;
 }
 
+int hollowPyramid() {
+    int n = 5;
+
+    // Upper Part
+    for (int i = 0; i < n; i++) {
+
+        // Leading spaces
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
+        }
+
+        // Left star
+        cout << "*";
+
+        // Middle spaces
+        for (int j = 0; j < 2 * i - 1; j++) {
+            cout << " ";
+        }
+
+        // Right star
+        if (i != 0) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Lower Part
+    for (int i = 1; i < n; i++) {
+
+        // Leading spaces
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+
+        // Left star
+        cout << "*";
+
+        // Middle spaces
+        for (int j = 0; j < 2 * (n - i) - 3; j++) {
+            cout << " ";
+        }
+
+        // Right star
+        if (i != n - 1) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
 int main(){
     pyramidPattern();
+    hollowPyramid();
     return 0;
 }
