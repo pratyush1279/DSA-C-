09dea34e6148e7c6e8bf3f17182f53fee5852935
pratyush1279 <pratyushpatwal@gmail.com>
2026-06-25 19:17:32 +0530

    message

--- Functions/function.cpp
+++ Functions/function.cpp
@@ -7,6 +7,18 @@ int sum(int a, int b) {
     return a + b; // 29
 }
 
+int sumOfDigits(int num){
+
+    int digSum = 0;
+
+    while(num > 0){
+        int lastDigit = num % 10;
+        num /= 10;
+        digSum += lastDigit;
+    }
+    return digSum;
+}
+
 int main() {
     int a = 5, b = 4;
 
@@ -14,6 +26,8 @@ int main() {
 
     cout << a << endl;
     cout << b << endl;
+
+    cout << sumOfDigits(1256);
 
     return 0;
 }
