9294b2fe83428998a9a66030f06acc3dbe73950e
pratyush1279 <pratyushpatwal@gmail.com>
2026-06-28 13:20:54 +0530

    message

--- Binary number/decimal_binary.cpp
+++ Binary number/decimal_binary.cpp
@@ -16,10 +16,24 @@ int decToBinary(int decNum) {
     return ans;
 }
 
+int BinToDecimal(int binNum){
+    int ans = 0;
+    int pow = 1;
+    
+    while (binNum > 0){
+        int rem = binNum % 10;
+        ans += (rem * pow);
+        binNum /= 10;
+        pow = pow * 2;
+    }
+    return ans;
+}
+
 int main() {
     int decNum = 5;
 
     cout << decToBinary(decNum) << endl;
+    cout << BinToDecimal(1100101) << endl;
 
     return 0;
 }
