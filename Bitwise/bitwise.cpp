f27d38827588aaac78d2f31d10f0d9d75c407673
pratyush1279 <pratyushpatwal@gmail.com>
2026-06-29 23:11:41 +0530

    message

---
+++ Bitwise/bitwise.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 10, b = 8;
+    cout << (a | b) << endl;
+    cout << (a & b) << endl;
+    cout << (a ^ b) << endl;
+    cout << (4 << 1) << endl;
+    return 0;
+}
